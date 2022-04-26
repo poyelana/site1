2f76ea09c0347a067e079d422e3d61b9728a9923
poyelana <segmogul@gmail.com>
2022-04-26 03:17:47 -0700

    Tuesday commit

--- footbet/footbet/urls.py
+++ footbet/footbet/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('newyear/', include("newyear.urls")),
+    path('newyear', include('newyear.urls')),
+  
 ]
